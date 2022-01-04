dab542af63a29807bebdd9f0b51df6e6c18eadab
Diego Herrera <daherre0@gmail.com>
2022-01-04 18:32:03 -0500

    Problema 4 resuelto

--- problema_4.py
+++ problema_4.py
@@ -3,3 +3,13 @@ de 3 imprima "Fizz" en lugar del número y para los múltiplos de 5 imprima "Buz
 números que son múltiplos de ambos imprima "FizzBuzz". """
 
 
+for i in range(1, 100):
+    if (i % 3 == 0 and i % 5 == 0):
+        print('FizzBuzz')
+    elif (i % 3 == 0):
+        print('Fizz')
+    elif  (i % 5 == 0):
+        print('Buzz')
+    else:
+        print(i)
+
